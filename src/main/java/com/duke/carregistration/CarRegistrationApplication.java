a129838ac942520b6581bc244554dbad80af47db
duke <dukekunyura@gmail.ru>
2023-03-05 23:02:01 +0300

    init and add SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) and add <version>${project.parent.version}</version> in pom.xml

---
+++ src/main/java/com/duke/carregistration/CarRegistrationApplication.java
@@ -0,0 +1,14 @@
+package com.duke.carregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class CarRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRegistrationApplication.class, args);
+	}
+
+}
