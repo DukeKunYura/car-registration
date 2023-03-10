c7292fffbb0feca8781598f8f45df3bf7169695b
duke <dukekunyura@gmail.ru>
2023-03-11 00:50:02 +0300

    add PersonDTO

---
+++ src/main/java/com/duke/carregistration/dto/PersonDTO.java
@@ -0,0 +1,11 @@
+package com.duke.carregistration.dto;
+
+import java.util.UUID;
+
+public class PersonDTO {
+    private UUID id;
+    private String passportNumber;
+    private String firstName;
+    private String surname;
+    private String patronymic;
+}
