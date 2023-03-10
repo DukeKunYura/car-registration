901c18375cebe3ee3f2cba90b1f7b54b1a3c2671
duke <dukekunyura@gmail.ru>
2023-03-11 00:29:07 +0300

    fix error db

--- src/main/java/com/duke/carregistration/entity/Person.java
+++ src/main/java/com/duke/carregistration/entity/Person.java
@@ -8,7 +8,7 @@ import org.hibernate.annotations.GenericGenerator;
 import java.util.UUID;
 
 @Entity
-@Table(name = "person")
+@Table(name = "persons")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Person {
