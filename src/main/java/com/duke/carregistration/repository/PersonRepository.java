4af5a369d7aeabb2b0aaf6cfccfb3a97a4b2748d
dukekunyura <dukekunyura@gmail.com>
2023-04-12 16:13:31 +0300

    add delete/update person and car

--- src/main/java/com/duke/carregistration/repository/PersonRepository.java
+++ src/main/java/com/duke/carregistration/repository/PersonRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
+
 @Repository
 public interface PersonRepository extends JpaRepository<Person, UUID> {
     Person findByPassportNumber(String passportNumber);
+
     @Query("select p from Person p left join fetch p.cars where p.id = :id")
     Person findPersonByIdWithCars(UUID id);
 }
